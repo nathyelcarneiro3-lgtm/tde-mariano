f694a86c93074a28c5ff6d478dfadc2a5ab42a02
nathyelcarneiro <nathyelcarneiro3@gmail.com>
2026-06-01 18:19:32 -0300

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.ts';
-import { AdminEventosComponent } from './components/admin-eventos/admin-eventos.ts';
-import { CadastroEventoComponent } from './components/cadastro-evento/cadastro-evento.ts';
+import { LoginComponent } from './components/login/login'; // Removido .ts
+import { AdminEventosComponent } from './components/admin-eventos/admin-eventos'; // Removido .ts
+import { CadastroEventoComponent } from './components/cadastro-evento/cadastro-evento'; // Removido .ts
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: AdminEventosComponent },
   { path: 'cadastro-evento', component: CadastroEventoComponent }
-];
+]
